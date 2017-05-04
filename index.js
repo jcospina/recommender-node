cfeb2e99f3b24f0388f6959b223193b7e24d6cf7
Juan Ospina <juan.ospina.quintero@gmail.com>
2017-05-04 14:44:49 -0500

    Load dataset using promises

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 /**
  * Created by Juan Camilo Ospina on 3/05/17.
  */
-module.exports = function(number, exp) {
-    return Math.pow(number, exp);
-};
+
+var recommender = {};
+
+const fs = require('fs');
+
+recommender.loadDataset = function (path) {
+    return new Promise(function (resolve, reject) {
+        fs.readFile(path, "utf8", function (error, data) {
+            if (!error) {
+                data = d3.csvParse(data);
+                var dataframe = jd.dfFromObjArray(data);
+                var interaction_matrix = dataframe.pivot('item', 'rating');
+                resolve(interaction_matrix);
+            } else {
+                reject(error);
+            }
+        });
+    });
+}
+
+module.exports = recommender;
