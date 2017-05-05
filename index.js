d9ca01d86dd5f1dde17da93b90b49f6fb005d579
Juan Ospina <juan.ospina.quintero@gmail.com>
2017-05-05 10:14:44 -0500

    Node recommender v1.0.2

--- index.js
+++ index.js
@@ -2,23 +2,49 @@
  * Created by Juan Camilo Ospina on 3/05/17.
  */
 
-var recommender = {};
+var nodeRecommender = {};
 
-const fs = require('fs');
+var fs = require('fs');
+var datamodel = require('./lib/datamodel');
+var recommender = require('./lib/recommender');
 
-recommender.loadDataset = function (path) {
+
+nodeRecommender.setup = function (path, k, clusterPath) {
     return new Promise(function (resolve, reject) {
-        fs.readFile(path, "utf8", function (error, data) {
-            if (!error) {
-                data = d3.csvParse(data);
-                var dataframe = jd.dfFromObjArray(data);
-                var interaction_matrix = dataframe.pivot('item', 'rating');
-                resolve(interaction_matrix);
-            } else {
-                reject(error);
+        datamodel.loadDataset(path).then(
+            (data) => {
+                var users = data.users;
+                var items = data.items;
+                datamodel.getClusters(data.matrix, k).then(
+                    (clusters) => {
+                        var fileData = {
+                            users: users.colArray(),
+                            items: items.values,
+                            clusters: clusters
+                        }
+                        fs.writeFile(clusterPath, JSON.stringify(fileData), function (err) {
+                            if (!err) {
+                                resolve(users.colArray());
+                            } else {
+                                reject(err);
+                            }
+                        });
+                    }
+                ).catch((err) => reject(err));
             }
-        });
+        ).catch((err) => reject(err));
+    });
+
+}
+
+nodeRecommender.recommend = function (userId, n, clusterPath) {
+    return new Promise(function (resolve, reject) {
+        recommender.recommend(userId, n, clusterPath).then(
+            (items) => {
+                resolve(items);
+            }
+        ).catch(err => reject(err));
     });
 }
 
-module.exports = recommender;
+module.exports = nodeRecommender;
